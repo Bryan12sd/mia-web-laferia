d6578e4eaec8202f3e6f31a42c9216866f6f20ea
Bryan Cordero <bryanzeta17@gmail.com>
2026-08-27 19:39:33 -0500

    feat: implement forecasts feature with listing, filtering, and dashboard integration

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views.login.index import login_view, logout_view
 from .views.dashboard.index import dashboard_view
+from .views.forecasts.index import forecast_list
 
 
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('dashboard/', dashboard_view, name='dashboard'),
+    path('pronosticos/', forecast_list, name='forecasts')
 ]
