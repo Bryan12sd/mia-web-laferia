e335265752c189a3b63390970f284de009e49f4a
Bryan Cordero <bryanzeta17@gmail.com>
2026-08-28 15:03:59 -0500

    feat: update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS for improved security and local development

--- mia_web_laferia/settings.py
+++ mia_web_laferia/settings.py
@@ -27,7 +27,16 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".ngrok-free.dev",
+    "localhost",
+    "127.0.0.1",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://escapade-favorable-crevice.ngrok-free.dev",
+    "https://*.ngrok-free.dev",
+]
 
 
 # Application definition
